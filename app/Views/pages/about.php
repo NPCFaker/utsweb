07d07f59c9470f02caefbee37e27431386e1523a
NPCFaker <derrydavinci@gmail.com>
2021-10-09 03:01:00 +0700

    pemrograman web ii code igniter blog

---
+++ app/Views/pages/about.php
@@ -0,0 +1,9 @@
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1>About Me</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet inventore eligendi quia cumque harum corporis reiciendis hic vitae sequi similique, quae commodi possimus eum illo id, voluptatibus dolorum facere temporibus.</p>
+        </div>
+    </div>
+</div>
+
---
+++ app/Views/pages/home.php
@@ -0,0 +1,10 @@
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1>HELLO WOLRD!!</h1>
+            <?php
+            dd($tes);
+            ?>
+        </div>
+    </div>
+</div>
