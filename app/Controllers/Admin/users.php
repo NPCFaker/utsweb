2bba47f90c7c24f1de34e10cb25046c278900a97
NPCFaker <derrydavinci@gmail.com>
2021-10-08 22:11:18 +0700

    belajar controller

---
+++ app/Controllers/Admin/users.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers\admin;
+
+use App\Controllers\basecontroller;
+
+class Users extends BaseController
+{
+    public function index()
+    {
+        echo "Ini contoller Users methodnya Index";
+    }
+}
